dd408f6be26c542e89f0ef54f40cf72b46373f0a
Tammie Chung <tammie.chung11@gmail.com>
2019-12-10 11:05:38 -0500

    refactor reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,7 +1,14 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
 const initialState = {
   articles: []
 };
 
 export default function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: [...state.articles, action.payload]
+    })
+  }
   return state;
 };
