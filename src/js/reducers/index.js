812b8b450ca6afd4d2cdc9fdebd32ab06d31c8a8
Tammie Chung <tammie.chung11@gmail.com>
2019-12-10 13:27:04 -0500

    refactor to use saga

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_ARTICLE, DATA_LOADED } from '../constants/action-types';
+import { ADD_ARTICLE, DATA_LOADED, API_ERRORED } from '../constants/action-types';
 
 const initialState = {
   articles: [],
@@ -12,6 +12,10 @@ export default function rootReducer(state = initialState, action) {
 
   if (action.type === DATA_LOADED) {
     return {...state, remoteArticles: [...state.remoteArticles, ...action.payload]}
+  }
+
+  if (action.type === API_ERRORED) {
+    return {...state, remoteArticles: [...state.remoteArticles, `There was an error: ${action.payload}`]}
   }
 
   return state;
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,13 +1,18 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from '../reducers/index';
-import { forbiddenWordsMiddleware } from '../middleware/index';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers/index";
+import { forbiddenWordsMiddleware } from "../middleware";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
+
+const initialiseSagaMiddleware = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, initialiseSagaMiddleware))
 );
+
+initialiseSagaMiddleware.run(apiSaga);
 
 export default store;
