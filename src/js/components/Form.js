a85fa59638c70a3bca5af370ebc3ea664ab71d6a
Tammie Chung <tammie.chung11@gmail.com>
2019-12-10 11:52:16 -0500

    add form component

---
+++ src/js/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { connect } from "react-redux";
+import { addArticle } from "../actions/index";
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addArticle: article => dispatch(addArticle(article))
+  };
+};
+
+const ConnectedForm = (props) => {
+
+  const [title, setTitle] = useState('');
+
+  const handleChange = event => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const submitTitle = title;
+    props.addArticle(submitTitle);
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">SAVE</button>
+    </form>
+  );
+}
+
+const Form = connect(null, mapDispatchToProps) (ConnectedForm);
+
+export default Form;
